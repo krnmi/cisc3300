17c7322760ea0a192e848730fe69388db0866596
ronece <113269553+krnmi@users.noreply.github.com>
2025-03-06 17:25:40 -0500

    Add files via upload

---
+++ in-class-15/index.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app;
+
+require "./app/controllers/PostController.php";
+
+use controllers\PostController;
+
+class Router {
+    public function handleRoutes() {
+        $url = strtok($_SERVER["REQUEST_URI"], '?');
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        $postController = new PostController();
+
+        if ($url === '/api/posts' && $method === 'GET') {
+            $postController->getPosts();
+        } elseif ($url === '/api/posts' && $method === 'POST') {
+            $postController->savePost();
+    
+        }
+    }
+}
