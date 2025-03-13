48e17c1b5492e59a3257b6619e808dda6028b37b
ronece <113269553+krnmi@users.noreply.github.com>
2025-03-13 14:47:42 -0400

    Update controller.php

--- homework 7/Controller/controller.php
+++ homework 7/Controller/controller.php
@@ -8,9 +8,6 @@ function handleFormSubmission($title, $description)
     } elseif (strlen($description) <= 10) {
         $response['message'] = 'the descriptions should be longer than 10 characters.';
     } else {
-        $safeTitle = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
-        $safeDescription = htmlspecialchars($description, ENT_QUOTES, 'UTF-8');
-
         $response['success'] = true;
         $response['message'] = 'note submitted!';
     }
